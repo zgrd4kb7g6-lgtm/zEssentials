590315dff424be7e97ee5288b5819c2b4ff46c87
zgrd4kb7g6-lgtm <zgrd4kb7g6@privaterelay.appleid.com>
2026-07-05 21:18:09 +0300

    Update LinkManager.java

--- DiscordBot/src/main/java/fr/maxlego08/essentials/bot/link/LinkManager.java
+++ DiscordBot/src/main/java/fr/maxlego08/essentials/bot/link/LinkManager.java
@@ -31,13 +31,29 @@ public class LinkManager extends ListenerAdapter {
         EmbedBuilder builder = instance.getConfiguration().getLink().embed().toEmbed();
 
         var config = instance.getConfiguration().getLink().button();
-        Button action = new ButtonImpl(BUTTON_LINK_NAME, config.name(), config.style(), config.disabled(), config.emoji() == null ? null : Emoji.fromUnicode(config.emoji()));
 
-        textChannel.sendMessageEmbeds(builder.build()).addActionRow(action).queue();
+        Button action = new ButtonImpl(
+                BUTTON_LINK_NAME,
+                config.name(),
+                config.style(),
+                config.disabled(),
+                config.emoji() == null ? null : Emoji.fromUnicode(config.emoji())
+        );
+
+        textChannel.sendMessageEmbeds(builder.build())
+                .addActionRow(action)
+                .queue();
     }
 
     @Override
     public void onButtonInteraction(@NotNull ButtonInteractionEvent event) {
+
+        // 🔥 FEATURE TOGGLE CHECK (IMPORTANT)
+        if (!instance.getFeatureManager().isEnabled("link-system")) {
+            event.reply("Link system is currently disabled.").setEphemeral(true).queue();
+            return;
+        }
+
         if (event.getComponentId().equals(BUTTON_LINK_NAME)) {
             createCode(event, event.getGuild(), event.getUser());
         }
@@ -49,48 +65,78 @@ public class LinkManager extends ListenerAdapter {
         var storage = instance.getStorageManager();
 
         storage.isAccountLinked(user.getIdLong(), isLinked -> {
+
             if (isLinked) {
-
                 event.reply(config.messages().already()).setEphemeral(true).queue();
-            } else {
-
-                var optional = getCode(user.getIdLong());
-
-                // If code already exists
-                if (optional.isPresent()) {
-                    var code = optional.get();
-                    replyCode(code.code(), event);
-
-                    storage.insertLog(DiscordAction.ASK_CODE, null, null, user.getEffectiveName(), user.getIdLong(), code.code());
-
-                    this.log(guild, config.log().channel(), config.log().ask()
-                            .replace("%name%", user.getName())
-                            .replace("%code%", code.code())
-                            .replace("%id%", String.valueOf(user.getIdLong()))
-                    );
-
-                    return;
-                }
-
-                // Otherwise, we will create one
-                String generatedCode = UUID.randomUUID().toString().replace("-", "").substring(0, 16);
-                DiscordCodeDTO newCode = new DiscordCodeDTO(generatedCode, user.getIdLong(), user.getName());
-                replyCode(generatedCode, event);
-                storage.saveCode(newCode);
-                storage.insertLog(DiscordAction.CREATE_CODE, null, null, user.getEffectiveName(), user.getIdLong(), generatedCode);
-
-                this.log(guild, config.log().channel(), config.log().create()
-                        .replace("%name%", user.getName())
-                        .replace("%code%", generatedCode)
-                        .replace("%id%", String.valueOf(user.getIdLong()))
-                );
+                return;
             }
+
+            var optional = getCode(user.getIdLong());
+
+            // If code already exists
+            if (optional.isPresent()) {
+                var code = optional.get();
+
+                replyCode(code.code(), event);
+
+                storage.insertLog(
+                        DiscordAction.ASK_CODE,
+                        null,
+                        null,
+                        user.getEffectiveName(),
+                        user.getIdLong(),
+                        code.code()
+                );
+
+                this.log(
+                        guild,
+                        config.log().channel(),
+                        config.log().ask()
+                                .replace("%name%", user.getName())
+                                .replace("%code%", code.code())
+                                .replace("%id%", String.valueOf(user.getIdLong()))
+                );
+
+                return;
+            }
+
+            // Otherwise create new code
+            String generatedCode = UUID.randomUUID().toString().replace("-", "").substring(0, 16);
+
+            DiscordCodeDTO newCode = new DiscordCodeDTO(
+                    generatedCode,
+                    user.getIdLong(),
+                    user.getName()
+            );
+
+            replyCode(generatedCode, event);
+
+            storage.saveCode(newCode);
+
+            storage.insertLog(
+                    DiscordAction.CREATE_CODE,
+                    null,
+                    null,
+                    user.getEffectiveName(),
+                    user.getIdLong(),
+                    generatedCode
+            );
+
+            this.log(
+                    guild,
+                    config.log().channel(),
+                    config.log().create()
+                            .replace("%name%", user.getName())
+                            .replace("%code%", generatedCode)
+                            .replace("%id%", String.valueOf(user.getIdLong()))
+            );
         });
     }
 
     private void log(Guild guild, long channelId, String message) {
 
         var channel = guild.getTextChannelById(channelId);
+
         if (channel == null) {
             System.err.println("Channel " + channelId + " not found");
             return;
@@ -100,8 +146,12 @@ public class LinkManager extends ListenerAdapter {
     }
 
     private void replyCode(String code, ButtonInteractionEvent event) {
+
         var config = instance.getConfiguration().getLink();
-        event.reply(config.messages().code().replace("%code%", code)).setEphemeral(true).queue();
+
+        event.reply(config.messages().code().replace("%code%", code))
+                .setEphemeral(true)
+                .queue();
     }
 
     private Optional<DiscordCodeDTO> getCode(long userId) {
